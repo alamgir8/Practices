4b24744feb64b297ed89106fded40938349cbef0
alamgir8 <ahossain38900@gmail.com>
2021-01-16 22:55:26 +0600

    javascripts module

---
+++ Module-12/condition.js
@@ -0,0 +1,21 @@
+var busVara = 100;
+if(busVara > 50){
+    console.log("I will go by bus");
+}
+else{
+    console.log("I will go there by train");
+}
+
+var trainVara =100
+var motorVara = 500;
+
+if (trainVara <150 || motorVara >500) {
+    console.log("I will go there by Train");            
+}
+else if (motorVara <500.1) {
+    console.log("I will go there by motor");
+}
+ else {    
+    console.log("I will go there by Bus");
+    
+}
