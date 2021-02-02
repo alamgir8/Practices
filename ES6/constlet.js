bf320993cce620cf9f51d5568ae74e8d3c4413dd
alamgir8 <ahossain38900@gmail.com>
2021-02-02 17:54:50 +0600

    updated

---
+++ ES6/constlet.js
@@ -0,0 +1,6 @@
+//const is short form of constant ,Constants are block-scoped, much like variables declared using the let keyword. The value of a constant can't be changed through reassignment, and it can't be redeclared.
+
+const number = [24,25, 50];
+
+number.push(12);
+console.log(number);
